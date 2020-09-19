5db29bd5af39f7b173d67824f387d86c7db999eb
Jailine Gonzalez <jaig1500@gmail.com>
2020-09-18 19:19:10 -0600

    Ejercicios Resueltos

--- sumatoria.py
+++ sumatoria.py
@@ -1,6 +1,11 @@
 def sumatoria_cubica(n):
-    raise NotImplementedError()
+	suma = 0
+	for i in range(1, n+1):
+		for j in range(1, i+1):
+			for k in range(j, i+j+1):
+				suma = suma + 1
+	return suma			
 
 def sumatoria_constante(n):
-    raise NotImplementedError()
-
+	suma = int((n*(n+1)*(n+2))/3)
+	return suma
